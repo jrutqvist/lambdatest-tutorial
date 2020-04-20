5296fec2ef0e9a5ed0d3cf3683b422d9cf50dd57
Pontus Broberg <pontus.broberg95@gmail.com>
2020-04-20 13:44:01 +0200

    added index.js file

---
+++ steps/index.js
@@ -0,0 +1,47 @@
+
+const USERNAME = '{yourUserName}';
+const KEY = '{yourAccesKey}';
+
+const webdriver = require('selenium-webdriver');
+const GRID_HOST = 'hub.lambdatest.com/wd/hub';
+const gridUrl = 'https://' + USERNAME + ':' + KEY + '@' + GRID_HOST;
+
+function testButtonFunction() {
+    // Setup Input capabilities
+    const capabilities = {
+        platform: 'windows 10',
+        browserName: 'chrome',
+        version: '76.0',
+        resolution: '1280x800',
+        network: true,
+        visual: true,
+        console: true,
+        video: true,
+        name: 'Test click',
+        build: 'First build'
+    }
+
+    const driver = new webdriver.Builder()
+        .usingServer(gridUrl)
+        .withCapabilities(capabilities)
+        .build();
+    
+    // navigate to a url, click the button and get title of next page.
+    driver.get("{yourURL}").then(function() {
+        driver.findElement(webdriver.By.linkText("click me!")).click().then(function() {
+            driver.getTitle().then(function(title) {
+                setTimeout(function() {if(title == 'secondpage') {
+                        console.log('Passed');
+                        driver.executeScript('lambda-status=passed');
+                        driver.quit();
+                    } else {
+                        console.log('failed');
+                        driver.executeScript('lambda-status=failed');
+                        driver.quit();
+                    }
+                }, 5000);
+            });
+        });
+    });
+}
+testButtonFunction();
